dec032a2066ed51935e0253f083120dcbbb5ebab
kksumm123 <kksumm123@gmail.com>
2021-09-07 21:22:06 +0900

    MySQL 닷넷모노 추가해서 커밋

---
+++ MySQL/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace MySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=localhost;Database=test;Uid=root;Pwd=zzz;";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Tab1 WHERE Id>=2";
+
+                //ExecuteReader를 이용하여
+                //연결 모드로 데이타 가져오기
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Console.WriteLine("{0}: {1}", rdr["Id"], rdr["Name"]);
+                }
+                rdr.Close();
+            }
+        }
+    }
+}
